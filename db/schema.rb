bc7a33c707c285f1621289aaee231300388c2223
franco9700 <francisco.iram97@gmail.com>
2020-04-08 12:38:18 -0700

    Stripe set up and add stripe fields to sign up view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_221711) do
+ActiveRecord::Schema.define(version: 2020_04_08_190709) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
